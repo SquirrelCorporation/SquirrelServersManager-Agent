18002cc51a863612be7acb79a387a902f9bff609
manu <manu@bankin.com>
2024-03-15 15:22:39 +0100

    commit

--- install.sh
+++ install.sh
@@ -121,7 +121,9 @@ if [ "${INSTALL_AGENT}" = true ]; then
   echo "##### Start agent..."
   # START AGENT
   pm2 start -f ./build/agent.js
-  eval "$(command pm2 startup | grep startup)"
+  SUB=$(command pm2 startup | grep startup)
+  echo "$SUB"
+  eval "$SUB"
   if [ $? == 0 ]; then
    echo "pm2 startup installed"
   else
